6fdfe85523b563bc628207d525c140c2c0495ba6
AdrielSantoss <adrieldossantos7@gmail.com>
2022-05-12 00:29:55 -0300

    Backend: Adiciona novas configurações e dependencias.

--- backend/configs/server.ts
+++ backend/configs/server.ts
@@ -1,3 +1,12 @@
-const app = require("express")();
+const app = require('express')(),
+    bodyParser = require('body-parser'),
+    consign = require('consign'),
+    cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+    .use(bodyParser.json())
+    .use(cors());
+
+consign().include('app/routes').include('app/controllers').into(app);
 
 module.exports = app;
