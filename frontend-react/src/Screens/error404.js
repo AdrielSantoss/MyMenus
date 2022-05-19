e9abd9bf2ba08aa7c226184dd0cec17a539268ff
Andrey Marques <andreymarques822@gmail.com>
2022-05-19 01:58:52 -0300

    Front-end: Criação da pagina de erro caso usuario tente acessar uma pagina inexistente

---
+++ frontend-react/src/Screens/error404.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react'
+import './style/style.css'
+
+
+
+const Error404 = () => {
+    
+    return (
+        <Fragment>
+           <div>
+                <h3 id='error' className='is title'>Page Not Found</h3>
+                <img id='error404' src='https://hugocalixto.com.br/wp-content/uploads/sites/22/2020/07/error-404-1.png' alt='Error404 Paga not found'/>
+            </div> 
+        </Fragment>
+        
+    )
+
+}
+
+export default Error404
