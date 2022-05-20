8bb039046e9814adb01fdc946e6b0db9c0c001f9
AdrielSantoss <adrieldossantos7@gmail.com>
2022-05-20 20:11:33 -0300

    Backend: Adiciona as configurações de novas funcionalidades.

--- backend/configs/server.ts
+++ backend/configs/server.ts
@@ -1,15 +1,18 @@
 const app = require('express')(),
-    bodyParser = require('body-parser'),
-    consign = require('consign'),
-    cors = require('cors'),
-    db = require('./database');
+  bodyParser = require('body-parser'),
+  consign = require('consign'),
+  cors = require('cors'),
+  db = require('./database'),
+  validator = require('express-validator');
 
 app.db = db;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-    .use(bodyParser.json())
-    .use(cors());
+app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(cors())
+  .use(validator());
 
-consign().include('app/routes').include('app/controllers').into(app);
+consign().include('app/routes').include('app/controllers').include('app/repositories').into(app);
 
 module.exports = app;
