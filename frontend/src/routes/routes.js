856f1404dd898bad3df9a90dcf87ead1a23bcc42
Andrey Marques <andreymarques822@gmail.com>
2022-05-22 22:42:47 -0300

    Front-end: Rota da pagina de recuperação de senha criada

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -3,6 +3,7 @@ import Error404 from '../screens/error404';
 import PrincipalPage from '../screens/mainScreen';
 import SignPage from '../screens/signScreen';
 import LoginPage from '../screens/loginScreen';
+import ForgotPage from '../screens/forgotpasswordScreen';
 
 const RouterPages = () => {
   return (
@@ -12,6 +13,7 @@ const RouterPages = () => {
         <Route exact path="*" element={<Error404 />} />
         <Route exact path="/sign" element={<SignPage />} />
         <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/forgotPassword" element={<ForgotPage />} />
       </Routes>
     </BrowserRouter>
   );
