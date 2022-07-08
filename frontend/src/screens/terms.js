77d585555c54ec62bf07e5d57f0582be44ec8bbf
Andrey Marques (Cobra) <97910870+andreymarques1202@users.noreply.github.com>
2022-07-08 17:36:20 -0300

    Frontend: Criação dos termos de uso.

---
+++ frontend/src/screens/terms.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react';
+
+const terms = () => {
+    return(
+        <Fragment>
+            <h1 id="termos">Terms and Conditions</h1>
+            <p>ao aceitar os termos o usuario passa a ter responsabilidade sobre
+                o modo de uso do sistema.
+            </p>
+            <hr/>
+            <br/>
+            <p>A empresa não se responsabiliza por uso indevido do sistema.</p>
+            <hr/>
+            <br/>
+            <p>Qualquer ato ou violação dos termos é estreitamente considerado crime.</p>
+            <hr/>
+            <br/>
+        </Fragment>
+    )
+}
